3ac3f0a9ca8528071e0fd03f1606c820f95f68dc
guido <guido.sanz@hotmail.com>
2020-08-12 12:55:41 -0300

    primer commit

---
+++ probrandoGit/src/main/java/com/educacionit/ProbrandoGitApplication.java
@@ -0,0 +1,13 @@
+package com.educacionit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProbrandoGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProbrandoGitApplication.class, args);
+	}
+
+}
